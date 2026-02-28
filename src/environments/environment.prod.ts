220e52aec08e85daaee0b6e38e9975506acd125a
RanaMohamed24 <raanaamohamed695@gmail.com>
2026-02-28 13:34:17 +0200

    configure environment file replacement and edit angular.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://bookstore-backend-lake.vercel.app/',
+  apiUrl: 'https://bookstore-backend-lake.vercel.app/api',
   cloudinary: {
     cloudName: 'dp0a6oouy',
     apiKey: '673626195932557',
